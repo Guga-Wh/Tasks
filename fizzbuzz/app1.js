6ee5ec439678278108ac5741ae3c6ab80f80e5e7
Guga-Wh <gugazhorzholiani1@gmial.com>
2019-07-02 16:14:05 +0400

    Realy Finall version

--- fizzbuzz/app1.js
+++ fizzbuzz/app1.js
@@ -1,15 +1,19 @@
+let ch = 0
 for(j=1;j<=100;j++){
     
-    if(j === 1){
-        console.log('Equals to 1')
-    } else {
-        if (j%3 == 0){
+  if (j%3 == 0){
         console.log('fizz ')
-    } if (j%5 ==0) {
+        ch=1
+    } 
+  if (j%5 ==0) {
         console.log('buzz ')
+        ch=1
     }
+    if(ch == 0){
+        console.log(j)
+    }
+    ch = 0
     
-    }
 }
 
 
