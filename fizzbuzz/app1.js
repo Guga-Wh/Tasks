126c85e09fb9763fec071aefbf7afb4a22f50923
Guga-Wh <gugazhorzholiani1@gmial.com>
2019-07-02 15:21:33 +0400

    FizzBuzz task

---
+++ fizzbuzz/app1.js
@@ -0,0 +1,21 @@
+const x = [ ]
+var i 
+for( i=1;i<= 10;i++){
+    x[i]=i;
+}
+for(j=0;j<x.length;j++){
+    if(x[j] === 1){
+        console.log('Equals to 1')
+    } else {
+        if (x[j]%3 == 0){
+        console.log('fizz')
+    } if (x[j]%5 ==0) {
+        console.log('buzz')
+    }
+      if(x[j]%5 ==0 || x[j]%3 == 0){
+          console.log('fizz buzz')
+      } 
+    }
+}
+
+
