a84b9e514111847a9f187f8a69e8950dfeafb722
Guga-Wh <gugazhorzholiani1@gmial.com>
2019-07-05 00:06:23 +0400

    fixed for one element

--- findindex/find.js
+++ findindex/find.js
@@ -1,5 +1,13 @@
 x = []
 const findIndex = (Array,Sen) => {
+    if(Array.length ==1){
+        for(j=0;j<Sen.length;j++){
+            if( Array == Sen[j] ){  
+               return console.log(j)
+
+            }
+         }
+    } else    {
     for (i=0;i<Array.length;i++){
         const view = Array[i]
              for(j=0;j<Sen.length;j++){
@@ -8,20 +16,23 @@ const findIndex = (Array,Sen) => {
                         x.push(Array[i])
                       }
                 }
-         
-    }
-    for(t=0;t<x.length;t++){
-        const rem = x[t]
-        for(s=0;s<Array.length;s++){
-        if(rem == Array[s])
-            Array.splice(s,1)
         }
-    }
-    if(Array.length !== 0){
-        Array.forEach(element => {
-            console.log(element + ' This symbol does not exist in the second argument')
-        })
-    }
-}
+            for(t=0;t<x.length;t++){
+                const rem = x[t]
+                   for(s=0;s<Array.length;s++){
+                      if(rem == Array[s])
+                           Array.splice(s,1)
+                      }           
+                }
+            }
+   
+            if(Array.length >1 ){
+                Array.forEach(element => {
+                    console.log(element + ' This symbol does not exist in the second argument')
+                })
+            } else {
+                console.log(Array + ' This symbol does not exist in the second argument')
+            }
+ }
 
-findIndex(['x'],'sbBcI')
+findIndex('x','sbBxcI')
