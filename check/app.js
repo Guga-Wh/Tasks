6d884b67d0dc19ce2decd186ad2e7ec3c49dc3e9
Guga-Wh <gugazhorzholiani1@gmail.com>
2019-07-10 14:29:50 +0400

    check tasks done

---
+++ check/app.js
@@ -0,0 +1,28 @@
+var type
+var count = 0
+const test = (user) =>{
+       type = typeof (user)
+            if(type == 'object'){
+            console.log(type)
+        for(x in user){
+            count++       
+         }
+         if(count == 0){
+             console.log('There is empty value')
+         } else {
+             console.log(count)
+         }
+        } else {
+            console.log('There is no object value')
+        }
+    
+}
+
+
+
+s = 5
+b = 1
+let user1 = {}
+let user = {test(user1)} 
+ test(user)
+
